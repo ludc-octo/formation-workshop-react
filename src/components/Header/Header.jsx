1d99e3ee6dc864fc2e237006c0e68e6161b0cdfe
Ludwig Chieng <ludwig.chieng@octo.com>
2023-02-17 14:59:39 +0100

    feat: link color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import './header.css';
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+
+const generateNavLink = (currentRoute, linkPath, linkLabel) => {
+  console.log(currentRoute, linkPath)
+  return currentRoute === linkPath ? (
+    <>{ linkLabel }</>
+  ) : (
+    <Link to={linkPath}>{ linkLabel }</Link>
+  )
+}
 
 const Header = () => {
+  const router = useLocation();
+
   return (<>
     <figcaption>Les bases du dev web</figcaption>
     <nav>
       <ul>
-        <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/publier">Publier</Link></li>
-        <li><Link to="/articles">Articles</Link></li>
-        <li><Link to="/meteo">Météo</Link></li>
+        <li>{ generateNavLink(router.pathname, "/", 'Accueil') }</li>
+        <li>{ generateNavLink(router.pathname, "/publier", 'Publier') }</li>
+        <li>{ generateNavLink(router.pathname, "/articles", 'Articles') }</li>
+        <li>{ generateNavLink(router.pathname, "/meteo", 'Météo') }</li>
       </ul>
     </nav>
   </>);
